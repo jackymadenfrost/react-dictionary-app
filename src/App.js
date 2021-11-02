883e8cb05eb8cda98707dde3c234f16a2131150f
Jacky Madenfrost <jacky.madenfrost@gmail.com>
2021-11-02 15:56:56 +1000

    Added the search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <img src={logo} className="App-logo img-fluid" alt="logo"></img>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo"></img>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          {" "}
+          <small>Coded by Jacky Madenfrost</small>
+        </footer>
+      </div>
     </div>
   );
 }
