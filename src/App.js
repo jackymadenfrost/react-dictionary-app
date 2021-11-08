33ba92d63caf3d0c8195aeddea5865d90ae63db0
Jacky Madenfrost <jacky.madenfrost@gmail.com>
2021-11-08 19:49:40 +1000

    added default keyword

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
           <img src={logo} className="App-logo img-fluid" alt="logo"></img>
         </header>
         <main>
-          <Dictionary defaultkeyword="sunset" />
+          <Dictionary defaultkeyword="skateboarding" />
         </main>
         <footer className="App-footer">
           {" "}
